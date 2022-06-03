c7faa09e2f071fc7c11fa417ccc52f82068d8c69
Valentin <valentinlaptop>
2022-06-03 09:41:17 +0200

    nav

--- includes/header.php
+++ includes/header.php
@@ -1,31 +1,31 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="index.php">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Lundi</a>
+    <ul class="navbar-nav mr-auto container-fluid text-center">
+      <li class="nav-item col-1">
+        <a class="nav-link" href="index.php?lundi">Lundi</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Mardi</a>
+      <li class="nav-item col-1">
+        <a class="nav-link" href="index.php?mardi">Mardi</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Mercredi</a>
+      <li class="nav-item col-1">
+        <a class="nav-link" href="index.php?mercredi">Mercredi</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Jeudi</a>
+      <li class="nav-item col-1">
+        <a class="nav-link" href="index.php?jeudi">Jeudi</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Vendredi</a>
+      <li class="nav-item col-1">
+        <a class="nav-link" href="index.php?vendredi">Vendredi</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Samedi</a>
+      <li class="nav-item col-1">
+        <a class="nav-link" href="index.php?samedi">Samedi</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Dimanche</a>
+      <li class="nav-item col-1">
+        <a class="nav-link" href="index.php?dimanche">Dimanche</a>
       </li>
     </ul>
   </div>
